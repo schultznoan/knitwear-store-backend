a45f898a0ba749d6354d30434ce329dda4c53568
schultznoan <danchoo14@bk.ru>
2023-05-22 11:34:58 +0300

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb')
 const fs = require('fs')
 const express = require('express')
 
-const client = new MongoClient('mongodb+srv://danchoo14:bu3oYdOFLqbn7lVU@chat-bot.u2hsxra.mongodb.net/?retryWrites=true&w=majority', {
+const client = new MongoClient('mongodb+srv://danchoo14:GpB529gxyFuVAruq@cluster0.1cgp7tx.mongodb.net/?retryWrites=true&w=majority', {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
@@ -12,11 +12,39 @@ const client = new MongoClient('mongodb+srv://danchoo14:bu3oYdOFLqbn7lVU@chat-bo
 
 client.connect()
     .then(() => console.log('Mongo connection success'))
-    .catch(() => console.log('Mongo connection error'))
+    .catch((err) => console.log(err))
 
 const server = express()
 server.use(express.json())
 
+server.get('/categories', async (_, res) => {
+    try {
+        const categories = await client
+            .db('node-marketplace')
+            .collection('categories')
+            .find()
+            .toArray()
 
+        res.json(categories)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+server.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const products = await client
+            .db('node-marketplace')
+            .collection('products')
+            .find(id === 'all' ? undefined : { category: id })
+            .toArray()
+
+        res.json(products)
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 server.listen(3000)
